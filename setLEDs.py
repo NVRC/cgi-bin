3df2f241f94e741ed8114515a5291cc889ee44cf
root <nathaniel.charlebois@gmail.com>
2018-05-09 19:03:16 +0000

    Fixed dotstarlib_correction implementation

--- setLEDs.py
+++ setLEDs.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 import cgi
+import cgitb
 from dotstar import Adafruit_DotStar
 from led_drivers import nLevelLinearGradient as nllg
+from led_drivers import gradientHelpers as gh
+cgitb.enable()
 fs = cgi.FieldStorage()
 argString =""
 for key in fs.keys():
@@ -15,8 +18,9 @@ strip = Adafruit_DotStar(60)
 strip.begin()
 i = 0
 for key in range(0,60):
-	print(str(key)+' 0x%04x' % int(fs[str(key)].value,16))
-	strip.setPixelColor(i,int('0x%04x' % int(fs[str(key)].value,16),16))
+	temp = gh.dotstarlibalt_correction(fs[str(key)].value)
+	print(str(key)+' 0x%06x' % int(temp,16))
+	strip.setPixelColor(i,int('0x%06x' % int(temp,16),16))
 	i = i + 1
 strip.setBrightness(int(fs["brightness"].value,10))
 strip.show()
