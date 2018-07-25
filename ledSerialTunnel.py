a84ee4e709457ce45d3dec8fe4a848a41025c465
NVRC <nathaniel.charlebois@gmail.com>
2018-07-25 17:36:08 -0400

    fix

--- ledSerialTunnel.py
+++ ledSerialTunnel.py
@@ -7,7 +7,7 @@ class LedSerialTunnel(object):
         self._colorArray = ""
         self._count = 0
         self._ser = serial.Serial(self._port,9600)
-        self._ser.port = "COM5"
+        self._ser.port = self._port
         self._ser.baudrate = 9600
         self._ser.timeout = 1
         self._ser.setDTR(False)
