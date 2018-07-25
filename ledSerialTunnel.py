9dab82bd6dcdfc5868b11015bf4d8f98339cfe83
NVRC <nathaniel.charlebois@gmail.com>
2018-07-25 18:44:00 -0400

    test

--- ledSerialTunnel.py
+++ ledSerialTunnel.py
@@ -17,6 +17,7 @@ class LedSerialTunnel(object):
         self._ser.write(b'0')
         import time
         time.sleep(1)
+        print(len(struct.pack('>B', self._brightness)))
         self._ser.write(struct.pack('>B', self._brightness))
 
     def addColor(self, colorHex):
