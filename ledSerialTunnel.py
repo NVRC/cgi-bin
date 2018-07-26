a7756bbe1927c4420e3d357090220c52e9ce2701
NVRC <nathaniel.charlebois@gmail.com>
2018-07-26 17:39:21 -0400

    Functional testLST.py with arduino

--- ledSerialTunnel.py
+++ ledSerialTunnel.py
@@ -3,36 +3,40 @@ class LedSerialTunnel(object):
     def __init__(self, b):
         import serial
         import struct
+        import time
         self._brightness = b
-        self._port = "/dev/ttyUSB0"
+        self._port = "/dev/cu.wchusbserial1410"
         self._colorArray = ""
         self._count = 0
         self._ser = serial.Serial()
         self._ser.port = self._port
         self._ser.baudrate = 9600
-        self._ser.timeout = 1
+        self._ser.timeout = 2
         self._ser.open()
         #self._ser.setDTR(False)
         #self._ser.setRTS(False)
+        print('0\r\n'.encode('utf-8'))
+        self._ser.write('0\r\n'.encode('utf-8'))
+        time.sleep(0.1)
 
-        self._ser.write(b'0')
-        import time
-        time.sleep(1)
-        print(struct.pack('>B', self._brightness))
-        self._ser.write(struct.pack('>B', self._brightness))
 
+        print("{:02x}\r\n".format(self._brightness).encode('utf-8'))
+        self._ser.write("{:02x}\r\n".format(self._brightness).encode('utf-8'))
+        time.sleep(0.5)
     def addColor(self, colorHex):
+        import time
         self._colorArray += colorHex
         self._count = self._count + 1
         if self._count == 10:
+            self._colorArray += '\r\n'
             print("Adding a segment")
-            print(len(self._colorArray))
-            print(len(str(self._colorArray)))
-            self._ser.write(str(self._colorArray))
+            print(self._colorArray.encode('utf-8'))
+            self._ser.write(self._colorArray.encode('utf-8'))
+            time.sleep(0.5)
             self._colorArray = ""
             self._count = 0
-            import time
-            time.sleep(1)
+
+            time.sleep(0.5)
 
     def close(self):
         self._ser.close()
--- testLST.py
+++ testLST.py
@@ -2,7 +2,7 @@ from ledSerialTunnel import LedSerialTunnel
 
 lct = LedSerialTunnel(int(255))
 
-color = '00ff00'
+color = 'ff0000'
 count = 0
 while count < 60:
     lct.addColor(color)
