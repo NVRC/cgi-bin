d3bb2629ba412946c0b1473b82bb74400a75d046
NVRC <nathaniel.charlebois@gmail.com>
2018-11-05 09:20:47 -0500

    Adding support for non-static cmds and rate

--- __main__.py
+++ __main__.py
@@ -2,5 +2,11 @@
 import sys
 from ledSerialTunnelDisabledDTR import LedSerialTunnelDisabledDTR
 
-ldt = LedSerialTunnelDisabledDTR(sys.argv[2])
-ldt.setColorList(sys.argv[1])
+if len(sys.argv) == 3:
+    ldt = LedSerialTunnelDisabledDTR(sys.argv[2])
+    ldt.setColorList(sys.argv[1])
+elif len(sys.argv) == 5:
+    ldt = LedSerialTunnelDisabledDTR(sys.argv[2])
+    ldt.setCmd(sys.argv[3])
+    ldt.setRate(sys.argv[4])
+    ldt.setColorList(sys.argv[1])
--- ledSerialTunnelDisabledDTR.py
+++ ledSerialTunnelDisabledDTR.py
@@ -11,25 +11,48 @@ class LedSerialTunnelDisabledDTR(object):
         self._ser = serial.Serial()
         self._ser.port = self._port
         self._ser.baudrate = 9600
+        self._rate = 0
+        self._cmd = 0;
         self._ser.open()
         time.sleep(0.1)
-        self._ser.write('<0>'.encode('utf-8'))
-        time.sleep(0.1)
 
-        self._ser.write("<{:02x}>".format(self._brightness).encode('utf-8'))
-        time.sleep(0.1)
+
+
+
+
+
     def addColor(self, colorHex):
-
         self._colorArray += colorHex
         self._count = self._count + 1
         if self._count == 60:
             self.writeColors()
 
     def writeColors(self):
+        if self._rate != 0:
+            self._ser.write('<{:01x}>'.format(self._cmd).encode('utf-8'))
+            time.sleep(0.1)
+        else:
+            self._ser.write('<0>'.encode('utf-8'))
+            time.sleep(0.1)
+
+        if self._rate != 0:
+            self._ser.write('<{:03x}>'.format(self._rate).encode('utf-8'))
+            time.sleep(0.1)
+
+        self._ser.write('<{:02x}>'.format(self._brightness).encode('utf-8'))
+        time.sleep(0.1)
+
+
         self._colorArray += ">"
         self._ser.write(self._colorArray.encode('utf-8'))
         self._colorArray = "<"
         self._count = 0
+
+    def setRate(self, rate):
+        self._rate = rate
+
+    def setCmd(self, cmd):
+        self._cmd = cmd
 
     def isOpen(self):
         if self._ser.isOpen() == True:
